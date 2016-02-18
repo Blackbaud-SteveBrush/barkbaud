37656574b89d50dcf3f8a28ec9736f7ff94b75c6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2016-02-18 17:54:18 -0500

    Added connection to db.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var app,
-    express;
+    dbPath,
+    express,
+    http,
+    mongoose;
 
+
+http = require('http');
+mongoose = require("mongoose");
 express = require('express');
 app = express();
+
+
+dbPath = 'mongodb://stevebrush:steve883@ds011308.mongolab.com:11308/barkbaud';
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/ui'));
@@ -11,4 +21,13 @@ app.get('/', function (req, res) {
 });
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
+});
+
+
+mongoose.connect(dbPath, function (err, res) {
+    if (err) {
+        console.log('ERROR connecting to: ' + dbPath + '. ' + err);
+    } else {
+        console.log('Succeeded connected to: ' + dbPath);
+    }
 });
